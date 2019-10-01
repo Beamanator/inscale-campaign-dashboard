f60c5f993aecc677289c6c780c8466bf8e3494e9
Beamanator <dabeamanator@gmail.com>
2019-10-02 00:37:09 +0200

    import cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { render, cleanup } from "@testing-library/react";
+import { cleanup } from "@testing-library/react";
 import { App } from "./App";
 
 // @material-ui imports
