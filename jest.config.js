39e9a357e40d08ccdf601d5e9d984a7c2365158d
Beamanator <dabeamanator@gmail.com>
2019-10-02 01:02:36 +0200

    update jest config to try to cover more .js files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     setupFilesAfterEnv: ["./src/setupTests.js"],
     moduleFileExtensions: ["js"],
-    collectCoverageFrom: ["src/*.js", "!src/index.js"],
+    collectCoverageFrom: [
+        "**/*.{js,jsx}",
+        "!**/node_modules/**",
+        "!**/vendor/**",
+    ],
     verbose: true,
 };
