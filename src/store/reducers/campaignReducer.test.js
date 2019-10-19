22578853ee002ec23bee5f3ff1e47ccde9487908
Beamanator <dabeamanator@gmail.com>
2019-10-19 21:01:58 +0200

    test: adding init state test when local storage unavailable

--- src/store/reducers/campaignReducer.test.js
+++ src/store/reducers/campaignReducer.test.js
@@ -7,15 +7,16 @@ const initState = {
 };
 
 describe("campaign reducer", () => {
-    // Note: technically this one only works as below if there's no
-    // -> campaigns in local storage... I didn't have time to test
-    // -> ALL test cases :(
     it("should return the initial state", () => {
         // {} represents an empty action type (not needed for init load)
-        expect(campaignReducer(undefined, {})).toEqual({
-            campaigns: [],
-            campaignError: null,
-        });
+        expect(campaignReducer(undefined, {})).toEqual(initState);
+    });
+
+    it("should load state with error if local storage unavailable", () => {
+        delete window["localStorage"];
+
+        // {} represents an empty action type (not needed for init load)
+        expect(campaignReducer(undefined, {})).not.toBe(initState);
     });
 
     it("should handle action type CAMPAIGN_ADD", () => {
