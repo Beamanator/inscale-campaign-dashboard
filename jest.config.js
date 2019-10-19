eaf949297876372093af92916465492956a478fa
Beamanator <dabeamanator@gmail.com>
2019-10-19 16:22:26 +0200

    test: add extra flag for all folders (hopefully)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     setupFilesAfterEnv: ["./src/setupTests.js"],
     moduleFileExtensions: ["js"],
-    collectCoverageFrom: ["src/**.{js,jsx}"],
+    collectCoverageFrom: [
+        "src/**/*.{js,jsx}",
+        "!**/node_modules/**",
+        "!**/vendor/**",
+    ],
     verbose: true,
 };
